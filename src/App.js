0ba0a822e3446692d5b2bf5aefe68b228f695e63
Dick Kluis <dkluis@icloud.com>
2020-11-13 14:51:50 -0500

    Slight change to be able to test the auto deployment to the server after build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Boiler Plate React App
+          Place Holder for the Work Example React App
         </p>
         <a
           className="App-link"
